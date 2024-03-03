532b04a9287ac0b7cdadcf047ba7557f94473d6a
haziqishere <haziqhakimi02@gmail.com>
2024-03-04 01:25:17 +0800

    first commit

---
+++ helpers/api-util.js
@@ -0,0 +1,20 @@
+export async function getAllEvents() {
+    const response = await fetch('https://nextevent-a5540-default-rtdb.asia-southeast1.firebasedatabase.app/events.json');
+    const data = await response.json();
+
+    const events = [];
+
+    for (const key in data) {
+        events.push({
+            id: key,
+            ...data[key]
+        });
+    }
+
+    return events;
+}
+
+export async function getFeaturedEvents() {
+    const allEvents = await getAllEvents();
+    return allEvents.filter((event) => event.isFeatured);
+  }
