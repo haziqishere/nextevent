612b5219e73ac318fa1a6ae2a985ae503f74dd5a
Haziq Hakimi <haziqhakimi02@gmail.com>
2024-03-08 00:56:58 +0800

    add POST method to newsletter. When user click register, the email will be req in POST method. then the response will be to the console log

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,26 @@
+import { useRef, useState } from 'react';
+
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
     // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body:JSON.stringify({email: enteredEmail}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      //send data to API to res in api/newsletter.js
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -19,6 +33,7 @@ function NewsletterRegistration() {
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            ref = {emailInputRef} 
           />
           <button>Register</button>
         </div>
---
+++ pages/api/newsletter.js
@@ -0,0 +1,16 @@
+function handler(req, res) {
+    //if the operation is POST
+    if (req.method === 'POST') {
+        const userEmail = req.body.email; 
+
+        if (!userEmail || !userEmail.includes('@')) {
+            res.status(422).json({messge: 'Invalid email address.'});
+            return;    
+        }
+
+        console.log(userEmail);
+        res.status(201).json({message: 'Signed up!'});
+    } 
+}
+
+export default handler;
