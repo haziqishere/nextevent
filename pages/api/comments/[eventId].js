e4d729428f9e64cae87b598e728ca6e4732a7f44
Haziq Hakimi <haziqhakimi02@gmail.com>
2024-03-08 04:07:58 +0800

    1)add dummy database 2)can POST comment to database via API

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,44 @@
+function handler(req, res){
+    const eventId = req.query.eventId;
+
+    // if request method is POST
+    if (req.method === 'POST') {
+        const { email, name, text } = req.body;
+
+        // add server-side validation
+        if (
+            !email.includes('@') || 
+            !name || 
+            name.trim()=== '' || 
+            !text || 
+            text.trim() === ''
+        ) {
+            res.status(422).json({message: 'invalid input.'});  
+            return;
+        }
+
+        // console log data
+        console.log(email, name, text);
+        const newComment = {
+            id: new Date().toISOString(),
+            email,
+            name,
+            text
+        };
+        console.log(newComment);
+        res.status(201).json({message: 'Comment added', comment: newComment})
+
+    } 
+    
+    // if request method is GET
+    else if (req.method === 'GET') {
+        const dummyList = [
+            { id: 'c1', name: 'Max', comment: 'A first comment!' },
+            { id: 'c2', name: 'Manuel', comment: 'A second comment!' },
+        ];
+
+        res.status(200).json({ comments: dummyList });
+    }
+}
+
+export default handler;
