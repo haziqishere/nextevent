1d5dc5d0724e193dd9d69373a7ff7a05f1fc7dcb
Haziq Hakimi <haziqhakimi02@gmail.com>
2024-03-08 20:11:34 +0800

    connect to mongodb and able to POST email to mongodb via API

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     //if the operation is POST
     if (req.method === 'POST') {
         const userEmail = req.body.email; 
@@ -7,8 +9,20 @@ function handler(req, res) {
             res.status(422).json({messge: 'Invalid email address.'});
             return;    
         }
+        // Connect to mongodb database. Database password: TsQcfOKPty2q4KgZ
+        const client = await MongoClient.connect(
+            'mongodb+srv://haziq:TsQcfOKPty2q4KgZ@cluster0.vwkwzqm.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0'
+        );
+        // get concrete of the db we connected
+        const db = client.db();
 
-        console.log(userEmail);
+        // get access to collection
+        await db.collection('emails')
+        //  run our queries for example insertOne for adding new entries (called doc in mongodb). [need to be JSON]
+        .insertOne({email: userEmail});
+
+        client.close(); //close client 
+
         res.status(201).json({message: 'Signed up!'});
     } 
 }
