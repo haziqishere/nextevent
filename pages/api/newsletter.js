4ecd922e5058b9b379bad2d9913c123de798e4cc
Haziq Hakimi <haziqhakimi02@gmail.com>
2024-03-09 15:20:33 +0800

    added error handling by putting operation to connect to mongodb and inserting into mongodb into dedicated async function. Then call those function in try catch

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,22 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+        // Connect to mongodb database. Database password: TsQcfOKPty2q4KgZ
+        const client = await MongoClient.connect(
+            'mongodb+srv://haziq:TsQcfOKPty2q4KgZ@cluster0.vwkwzqm.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0'
+        );
+        return client;
+}
+
+async function insertDocument(client, document) {
+        // create a concrete of db
+        const db = client.db();
+        // get access to collection
+        await db.collection('newsletter')
+        //  run our queries for example insertOne for adding new entries (called doc in mongodb). [need to be JSON]
+        .insertOne(document);
+}
+
 async function handler(req, res) {
     //if the operation is POST
     if (req.method === 'POST') {
@@ -9,21 +26,25 @@ async function handler(req, res) {
             res.status(422).json({messge: 'Invalid email address.'});
             return;    
         }
-        // Connect to mongodb database. Database password: TsQcfOKPty2q4KgZ
-        const client = await MongoClient.connect(
-            'mongodb+srv://haziq:TsQcfOKPty2q4KgZ@cluster0.vwkwzqm.mongodb.net/events?retryWrites=true&w=majority&appName=Cluster0'
-        );
-        // get concrete of the db we connected
-        const db = client.db();
 
-        // get access to collection
-        await db.collection('newsletter')
-        //  run our queries for example insertOne for adding new entries (called doc in mongodb). [need to be JSON]
-        .insertOne({email: userEmail});
+        let client; // initialize client var so inserDocument can be parse even if error occur
 
-        client.close(); //close client 
-
-        res.status(201).json({message: 'Signed up!'});
+        try {
+            // connect to mongodb
+            client = await connectDatabase();
+        } catch(error) {
+            res.status(500).json({message: 'Connecting o the database failed'});
+            return;
+        }
+        
+        try {
+            await insertDocument(client, { email: userEmail });
+            client.close(); //close client 
+        } catch (error) {
+            res.status(500).json({message: 'Inserting data failed'});
+            return;
+        }
+        res.status(201).json({ message: 'Signed up!' });
     } 
 }
 
